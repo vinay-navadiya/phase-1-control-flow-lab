ad2042b9b328fd20298282b3ddcd0b8fc2de7a71
Vinay <vinaynavadiya@gmail.com>
2023-04-02 19:03:39 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+    if (distance <= 400){
+      return 'This one is on me!'
+    }
+    else if (distance >= 401 && distance <= 2000){
+      return 'That will be twenty bucks.'
+    }
+    else if (distance >= 2001 && distance <=2500){
+      return 'I will gladly take your thirty bucks.'
+    }
+    else if (distance >=2501){
+      return 'No can do.'
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+let response = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return ('Thank you so much.');
+    case "not as generous" :
+      return ('Thank you.');
+    case "thanks for everything" :
+      return ('Bye.');
+  }
 }
